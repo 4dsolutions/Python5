5ed4b5a69ac9ec183b21d2f94c04e127116311a1
Kirby Urner <kirby.urner@gmail.com>
2016-01-11 19:09:47 -0800

    starting with classes

---
+++ intro_classes.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 18:28:39 2016
+
+@author: Kirby Urner
+
+Intro to classes:  
+with thanks to PDX Code Guild, Instructor:  Tiffany
+"""
+
+from random import choice
+
+class Mammal:
+    
+    def __init__(self):
+        self.stomach = [ ]
+        
+    def eat(self, food):
+        self.stomach.append(food)
+        
+class Dog(Mammal):
+    
+    def __init__(self, name, breed):
+        super().__init__()
+        self.name = name
+        self.breed = breed
+        
+    def bark(self, how_many):  # expect int
+        return "Bark! " * how_many
+        
+class Die:  # as in dice
+    """model a die of n sides"""
+    
+    def __init__(self, sides = None):
+        if not sides:
+            self.sides = list(map(str, range(1,7)))
+        else:
+            self.sides = sides
+        self.current_value = self.sides[0]
+        
+    def roll(self):
+        """pick a side, any side..."""
+        self.current_value = choice(self.sides)
+        return self.current_value
+        
+    def __repr__(self):
+        return "Die with value {}".format(self.current_value)
+
+if __name__ == "__main__":    
+    puppy = Dog("Snoopy", "Beagle")
+    print(puppy.bark(3))
+    puppy.eat("steak!")
+    print("Inside puppy:", puppy.stomach)
+          
+    it, it1, it2, it3 = Die(), Die(), Die(), Die()
+    print((it2.roll(), it3.roll()))
+    print(it.roll())
+    print(it.roll())
+    print(it.roll())
+    print(it.roll())
+    print(it)
+    print("it:", it.__dict__)
+    print("it2:", it2.__dict__)
+
