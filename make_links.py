65779b22dee5351ebd57c888d361eac310cc9844
Kirby Urner <kirby.urner@gmail.com>
2016-01-31 21:36:30 -0800

    enhance make_links.py with namedtuple collection

--- make_links.py
+++ make_links.py
@@ -4,44 +4,49 @@ Created on Thu Jan 28 21:27:07 2016
 
 @author: kurner
 
-Compiled with assistance from <guild /> apprentices...
+A namedtuple is a subclass of tuple allowing for named
+columns and therefore dot notation access, treating 
+elements as named attributes.
 """
+from collections import namedtuple
 
+Bmk = namedtuple('Bookmark', 'place url')
 
 # tuple of tuples
 bookmarks = (
-    ("Intro to Programming @ Google Drive", "http://bit.ly/1QzO5CF"),
-    ("Intro to Programming @ DropBox", "http://bit.ly/1TrSJFB"),
-    ("Anaconda.org", "http://anaconda.org"),
-    ("Python.org", "http://python.org"),
-    ("Python Docs", "https://docs.python.org/3/"),
-    ("Obama wants $4 Bil to help with CompSci / GNU Math","http://www.msn.com/en-us/news/us/obama-wants-dollar4b-to-help-students-learn-computer-science/ar-BBoTN07?ocid=ansmsnnews11"),
-    ("Spaghetti Code", "http://c2.com/cgi/wiki?SpaghettiCode"),
-    ("Structured Programming", "http://c2.com/cgi/wiki?StructuredProgramming"),
-    ("Learn Python Through Public Data Hacking by David Beazley", "https://youtu.be/RrPZza_vZ3w"), 
-    ("Map of Languages", "http://archive.oreilly.com/pub/a/oreilly//news/languageposter_0504.html"),
-    ("XKCD", "http://xkcd.com"),
-    ("Django","https://docs.djangoproject.com/"),
-    ("PythonAnywhere","https://www.pythonanywhere.com/"),
-    ("CodeAcademy: Python","https://www.codecademy.com/learn/python"),
-    ("Unicode on Youtube", "https://www.youtube.com/watch?v=Z_sl99D2a18"),
-    ("In Defense of Ada", "http://www.grunch.net/synergetics/adaessay.html"),
-    ("Grace Hopper on Letterman", "https://www.youtube.com/watch?v=1-vcErOPofQ"),
-    ("The Mind of a Genius: John von Neumann", "https://www.youtube.com/watch?v=XZ9tt72feL8"),
-    ("World Domination meme", "https://www.google.com/search?q=linux+world+domination&safe=off&source=lnms&tbm=isch"),
-    ("Warriors of the Net", "https://www.youtube.com/watch?v=PBWhzz_Gn10"),
-    ("LAMP stack", "https://www.google.com/search?q=lamp+stack&safe=off&biw=787&bih=535&source=lnms&tbm=isch"),
-    ("LAMP stack (Wikipedia)","https://en.wikipedia.org/wiki/LAMP_(software_bundle)"),
-    ("In the Beginning was the Command Line", "http://c2.com/cgi/wiki?InTheBeginningWasTheCommandLine"),
-    ("First Wiki","http://c2.com/cgi-bin/wiki" ),
-    ("Financial Data Visualization","http://finviz.com/map.ashx"),
-    ("Human Development Index","http://hdr.undp.org/en/content/human-development-index-hdi"),
-    ("Inside The Eye","http://ngm.nationalgeographic.com/2016/02/evolution-of-eyes-text"),
-    ("Top 10 Languages to Learn in 2016", "https://youtu.be/Z56GLRXxh88"),
-    ("Pretty Good Python Summary by Derek Banas", "https://youtu.be/N4mEzFDjqtA"),
-    ("How To Think Like a Computer Scientist - Free Book","http://www.greenteapress.com/thinkpython/thinkCSpy.pdf"),
-    ("SQL Syntax", "https://www.google.com/search?q=sql+syntax&safe=off&biw=787&bih=535&source=lnms&tbm=isch")
+    Bmk("Intro to Programming @ DropBox", "http://bit.ly/1TrSJFB"),
+    Bmk("Anaconda.org", "http://anaconda.org"),
+    Bmk("Python.org", "http://python.org"),
+    Bmk("Python Docs", "https://docs.python.org/3/"),
+    Bmk("Spaghetti Code", "http://c2.com/cgi/wiki?SpaghettiCode"),
+    Bmk("Structured Programming", "http://c2.com/cgi/wiki?StructuredProgramming"),
+    Bmk("Map of Languages", "http://archive.oreilly.com/pub/a/oreilly//news/languageposter_0504.html"),
+    Bmk("XKCD", "http://xkcd.com"),
+    Bmk("Django","https://docs.djangoproject.com/"),
+    Bmk("PythonAnywhere","https://www.pythonanywhere.com/"),
+    Bmk("CodeAcademy: Python","https://www.codecademy.com/learn/python"),
+    Bmk("Unicode on Youtube", "https://www.youtube.com/watch?v=Z_sl99D2a18"),
+    Bmk("In Defense of Ada", "http://www.grunch.net/synergetics/adaessay.html"),
+    Bmk("Grace Hopper on Letterman", "https://www.youtube.com/watch?v=1-vcErOPofQ"),
+    Bmk("The Mind of a Genius: John von Neumann", "https://www.youtube.com/watch?v=XZ9tt72feL8"),
+    Bmk("World Domination meme", "https://www.google.com/search?q=linux+world+domination&safe=off&source=lnms&tbm=isch"),
+    Bmk("Warriors of the Net", "https://www.youtube.com/watch?v=PBWhzz_Gn10"),
+    Bmk("LAMP stack", "https://www.google.com/search?q=lamp+stack&safe=off&biw=787&bih=535&source=lnms&tbm=isch"),
+    Bmk("LAMP stack (Wikipedia)","https://en.wikipedia.org/wiki/LAMP_(software_bundle)"),
+    Bmk("In the Beginning was the Command Line", "http://c2.com/cgi/wiki?InTheBeginningWasTheCommandLine"),
+    Bmk("First Wiki","http://c2.com/cgi-bin/wiki" ),
+    Bmk("Financial Data Visualization","http://finviz.com/map.ashx"),
+    Bmk("Human Development Index","http://hdr.undp.org/en/content/human-development-index-hdi"),
+    Bmk("Inside The Eye","http://ngm.nationalgeographic.com/2016/02/evolution-of-eyes-text"),
+    Bmk("Top 10 Languages to Learn in 2016", "https://youtu.be/Z56GLRXxh88"),
+    Bmk("Pretty Good Python Summary by Derek Banas", "https://youtu.be/N4mEzFDjqtA"),
+    Bmk("How To Think Like a Computer Scientist - Free Book","http://www.greenteapress.com/thinkpython/thinkCSpy.pdf"),
+    Bmk("SQL Syntax", "https://www.google.com/search?q=sql+syntax&safe=off&biw=787&bih=535&source=lnms&tbm=isch")
     )
+    
+for bmk in bookmarks:
+    print(bmk.place, bmk.url, sep="\n") # using dot notation to get elements
+    print("-")
 
 page = """\
 <!DOCTYPE HTML>
@@ -64,8 +69,9 @@ html = """\
 """.lower()
 
 the_body = ""
-for place, url in sorted(bookmarks):
-    the_body += "<li><a href='{}'>{}</a></li>\n".format(url, place)
+for bmk in sorted(bookmarks):
+    # note the placeholder {0} accepts dot notation too, bmk being a namedtuple
+    the_body += "<li><a href='{0.url}'>{0.place}</a></li>\n".format(bmk)
 
 out = open("links.html", "w")
 print(page.format(html.format(the_body)), file=out)
