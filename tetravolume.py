4c4f37efc353c826aeab1e86ed63e28e0128652e
Kirby Urner <kirby.urner@gmail.com>
2018-05-14 14:34:10 -0700

    new martian math notebook

--- tetravolume.py
+++ tetravolume.py
@@ -3,21 +3,18 @@ Euler volume, modified by Gerald de Jong
 http://www.grunch.net/synergetics/quadvols.html
 Kirby Urner (c) MIT License
 
-Revisions History:
-April 9, 2017: phasing in Decimal type
-
 See:
 http://mathforum.org/kb/thread.jspa?threadID=2836546
 for explanation of quadrays, used for some unit tests
 """
 
+from math import sqrt as rt2
 from qrays import Qvector, Vector
 import sys
-from decimal import Decimal
 
-S3    = pow(Decimal(9)/Decimal(8), Decimal(0.5))
-root2 = pow(Decimal(2), Decimal(0.5))
-root5 = pow(Decimal(5), Decimal(0.5))
+S3    = pow(9/8, 0.5)
+root2 = rt2(2)
+root5 = rt2(5)
 
 class Tetrahedron:
     """
@@ -27,7 +24,7 @@ class Tetrahedron:
     """
 
     def __init__(self, a, b, c, d, e, f):
-        a,b,c,d,e,f = [Decimal(i) for i in (a,b,c,d,e,f)]
+        # a,b,c,d,e,f = [Decimal(i) for i in (a,b,c,d,e,f)]
         self.a, self.a2 = a, a**2
         self.b, self.b2 = b, b**2
         self.c, self.c2 = c, c**2
@@ -38,11 +35,11 @@ class Tetrahedron:
     def ivm_volume(self):
         ivmvol = ((self._addopen() 
                     - self._addclosed() 
-                    - self._addopposite())/2) ** Decimal(0.5)
+                    - self._addopposite())/2) ** 0.5
         return ivmvol
 
     def xyz_volume(self):
-        xyzvol = Decimal(1)/S3 * self.ivm_volume()
+        xyzvol = 1/S3 * self.ivm_volume()
         return xyzvol
 
     def _addopen(self):
@@ -84,29 +81,29 @@ def make_tet(v0,v1,v2):
                       (v0-v1).length(), (v1-v2).length(), (v2-v0).length())
     return tet.ivm_volume(), tet.xyz_volume()
 
-PHI = (1 + root5)/Decimal(2)
+PHI = (1 + root5)/2.0
 
-R = Decimal(0.5)
-D = Decimal(1.0)
-root3 = pow(Decimal(3), Decimal(0.5))
-root2 = pow(Decimal(2), Decimal(0.5))
+R = 0.5
+D = 1.0
+root3 = pow(3, .5)
+root2 = pow(2, .5)
 
 import unittest
 class Test_Tetrahedron(unittest.TestCase):
 
     def test_unit_volume(self):
         tet = Tetrahedron(D, D, D, D, D, D)
-        self.assertEqual(tet.ivm_volume(), Decimal(1), "Volume not 1")
+        self.assertEqual(tet.ivm_volume(), 1, "Volume not 1")
 
     def test_e_module(self):
         e0 = D
-        e1 = Decimal('3').sqrt() * PHI**-1
-        e2 = ((Decimal('5') - root5)/2).sqrt()
-        e3 = (Decimal('3') - root5)/2
-        e4 = (Decimal('5') - 2*root5).sqrt()
+        e1 = rt2(3) * PHI**-1
+        e2 = rt2((5 - root5)/2)
+        e3 = (3 - root5)/2
+        e4 = rt2(5 - 2*root5)
         e5 = 1/PHI
         tet = Tetrahedron(e0, e1, e2, e3, e4, e5)
-        self.assertTrue(Decimal(1/23) > tet.ivm_volume()/8 > Decimal(1/24), "Wrong E-mod")
+        self.assertTrue(1/23 > tet.ivm_volume()/8 > 1/24, "Wrong E-mod")
         
     def test_unit_volume2(self):
         tet = Tetrahedron(R, R, R, R, R, R)
@@ -117,51 +114,53 @@ class Test_Tetrahedron(unittest.TestCase):
         self.assertAlmostEqual(float(tet.ivm_volume()), 0.70710678)
 
     def test_right_tetra(self):
-        e = pow((root3/Decimal(2))**Decimal(2) + (root3/Decimal(2))**Decimal(2), Decimal(0.5))  # right tetrahedron
+        e = pow((root3/2)**2 + (root3/2)**2, 0.5)  # right tetrahedron
         tet = Tetrahedron(D, D, D, D, D, e)
-        self.assertAlmostEqual(tet.xyz_volume(), Decimal(1))
+        self.assertAlmostEqual(tet.xyz_volume(), 1)
 
     def test_quadrant(self):
         qA = Qvector((1,0,0,0))
         qB = Qvector((0,1,0,0))
         qC = Qvector((0,0,1,0))
-        tet = Tetrahedron(qA.length(), qB.length(), qC.length(), 
-                (qA-qB).length(), (qA-qC).length(), (qB-qC).length())
-        self.assertAlmostEqual(tet.ivm_volume(), Decimal(0.25)) 
+        tet = make_tet(qA, qB, qC) 
+        self.assertAlmostEqual(tet[0], 0.25) 
 
     def test_octant(self):
         x = Vector((0.5, 0,   0))
         y = Vector((0  , 0.5, 0))
         z = Vector((0  , 0  , 0.5))
-        tet = Tetrahedron(x.length(), y.length(), z.length(), 
-                (x-y).length(), (x-z).length(), (y-z).length())
-        self.assertAlmostEqual(tet.xyz_volume(), Decimal(1)/Decimal(6), 5) # good to 5 places
+        tet = make_tet(x,y,z)
+        self.assertAlmostEqual(tet[1], 1/6, 5) # good to 5 places
 
     def test_quarter_octahedron(self):
         a = Vector((1,0,0))
         b = Vector((0,1,0))
         c = Vector((0.5,0.5,root2/2))
-        tet = Tetrahedron(a.length(), b.length(), c.length(), 
-                (a-b).length(), (b-c).length(), (c-a).length())
-        self.assertAlmostEqual(tet.ivm_volume(), Decimal(1), 5) # good to 5 places  
+        tet = make_tet(a, b, c)
+        self.assertAlmostEqual(tet[0], 1, 5) # good to 5 places  
 
     def test_xyz_cube(self):
         a = Vector((0.5, 0.0, 0.0))
         b = Vector((0.0, 0.5, 0.0))
         c = Vector((0.0, 0.0, 0.5))
-        R_octa = Tetrahedron(a.length(), b.length(), c.length(), 
-                   (a-b).length(), (b-c).length(), (c-a).length()).xyz_volume()
-        self.assertAlmostEqual(6 * R_octa, Decimal(1), 4) # good to 4 places  
+        R_octa = make_tet(a,b,c) 
+        self.assertAlmostEqual(6 * R_octa[1], 1, 4) # good to 4 places  
 
     def test_s3(self):
         D_tet = Tetrahedron(D, D, D, D, D, D)
         a = Vector((0.5, 0.0, 0.0))
         b = Vector((0.0, 0.5, 0.0))
         c = Vector((0.0, 0.0, 0.5))
-        R_cube = 6 * Tetrahedron(a.length(), b.length(), c.length(), 
-                (a-b).length(), (b-c).length(), (c-a).length()).xyz_volume()
+        R_cube = 6 * make_tet(a,b,c)[1]
         self.assertAlmostEqual(D_tet.xyz_volume() * S3, R_cube, 4)
 
+    def test_martian(self):
+        p = Qvector((2,1,0,1))
+        q = Qvector((2,1,1,0))
+        r = Qvector((2,0,1,1))
+        result = make_tet(5*q, 2*p, 2*r)
+        self.assertAlmostEqual(result[0], 20, 7)
+        
 def command_line():
     args = sys.argv[1:]
     try:
